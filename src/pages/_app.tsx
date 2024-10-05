c27059c06392e1d856b6d9dd7805d86f07938fa3
Christian Kobril <christiankobril@gmail.com>
2024-10-05 12:39:58 +0200

    fix: remove rainbowkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import PrivyProviders from "@/components/providers";
 import "@/styles/style.scss";
 import { SharedStateProvider } from "@/utils/store";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";
 import PlausibleProvider from "next-plausible";
 import type { AppProps } from "next/app";
-import { WagmiConfig } from "wagmi";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
