388488f52a181ca5f9c6b63c76a571d0c1920b66
Christian Kobril <christiankobril@gmail.com>
2024-10-05 12:18:04 +0200

    chore: remove old providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import PrivyProviders from "@/components/providers";
-import { chains, wagmiConfig } from "@/providers";
 import "@/styles/style.scss";
 import { SharedStateProvider } from "@/utils/store";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
@@ -13,11 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <PlausibleProvider domain="example.com" trackOutboundLinks>
       <PrivyProviders>
         <SharedStateProvider>
-          <WagmiConfig config={wagmiConfig}>
-            <RainbowKitProvider chains={chains} modalSize="compact">
-              <Component {...pageProps} />
-            </RainbowKitProvider>
-          </WagmiConfig>
+          <Component {...pageProps} />
         </SharedStateProvider>
       </PrivyProviders>
     </PlausibleProvider>
